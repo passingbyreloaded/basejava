16fb3248e747e18972d1204adf0ed400e4502ca5
passingbyreloaded <voronayala@mail.ru>
2018-06-30 18:09:55 +0400

    implemented methods in class ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,49 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        return null;
+        return Arrays.stream(storage, 0, size)
+                .filter(r -> r.toString().equals(uuid))
+                .findFirst().orElse(null);
     }
 
     void delete(String uuid) {
+        int index = IntStream.range(0, size)
+                .filter(i -> uuid.equals(storage[i].toString()))
+                .findFirst().orElse(-1);
+
+        if (index > -1) {
+            IntStream.range(index, size - 1).forEach(i -> storage[i] = storage[i + 1]);
+            storage[size - 1] = null;
+            size--;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
