40a7b62a4f7ea2b6d520c9c8434aaad4a6d2b516
passingbyreloaded <voronayala@mail.ru>
2018-07-10 21:01:14 +0400

    SortedArrayStorage implementation

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -1,11 +1,12 @@
 import ru.javawebinar.basejava.model.Resume;
-import ru.javawebinar.basejava.storage.ArrayStorage;
+import ru.javawebinar.basejava.storage.SortedArrayStorage;
+import ru.javawebinar.basejava.storage.Storage;
 
 /**
  * Test for com.urise.webapp.storage.ru.javawebinar.basejava.storage.ArrayStorage
  */
 public class MainTestArrayStorage {
-    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    static final Storage ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
@@ -14,10 +15,13 @@ public class MainTestArrayStorage {
         r2.setUuid("uuid2");
         Resume r3 = new Resume();
         r3.setUuid("uuid3");
+        Resume r4 = new Resume();
+        r4.setUuid("uuid4");
 
         ARRAY_STORAGE.save(r1);
-        ARRAY_STORAGE.save(r2);
+        ARRAY_STORAGE.save(r4);
         ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(r2);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -1,33 +1,61 @@
 package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
+
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     public void clear() {
-
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     @Override
     public void save(Resume resume) {
-
+        int index;
+        if ((index = getIndex(resume.getUuid())) >= 0) {
+            System.out.println("The resume is already in the storage");
+        } else if (size == storage.length) {
+            System.out.println("Storage overflow");
+        } else if (size == 0) {
+            storage[size] = resume;
+            size++;
+        } else {
+            int indexForInsert = index * (-1) - 1;
+            System.arraycopy(storage, indexForInsert, storage, indexForInsert + 1, size - indexForInsert);
+            storage[indexForInsert] = resume;
+            size++;
+        }
     }
 
     @Override
     public void update(Resume resume) {
-
+        int index;
+        if ((index = getIndex(resume.getUuid())) >= 0) {
+            storage[index] = resume;
+        } else {
+            System.out.println("No such resume in the storage");
+        }
     }
 
     @Override
     public void delete(String uuid) {
-
+        int index;
+        if ((index = getIndex(uuid)) >= 0) {
+            IntStream.range(index, size - 1).forEach(i -> storage[i] = storage[i + 1]);
+            storage[size - 1] = null;
+            size--;
+        } else {
+            System.out.println("No such resume in the storage");
+        }
     }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     @Override
