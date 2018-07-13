0bc917a57339c5623a5b76232406c4658a486060
passingbyreloaded <voronayala@mail.ru>
2018-07-13 14:36:06 +0400

    storages refactored

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -2,6 +2,8 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
 
+import java.util.Arrays;
+
 public abstract class AbstractArrayStorage implements Storage {
 
     protected static final int CAPACITY = 10000;
@@ -23,5 +25,53 @@ public abstract class AbstractArrayStorage implements Storage {
 
     }
 
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void save(Resume resume) {
+        int index;
+        if ((index = getIndex(resume.getUuid())) >= 0) {
+            System.out.println("The resume is already in the storage");
+            return;
+        }
+        if (size == storage.length) {
+            System.out.println("Storage overflow");
+            return;
+        }
+        insertElement(resume, index);
+        size++;
+    }
+
+    public void delete(String uuid) {
+        int index;
+        if ((index = getIndex(uuid)) >= 0) {
+            fillDeletedElement(index);
+            storage[size - 1] = null;
+            size--;
+        } else {
+            System.out.println("No such resume in the storage");
+        }
+    }
+
+    public void update(Resume resume) {
+        int index;
+        if ((index = getIndex(resume.getUuid())) >= 0) {
+            storage[index] = resume;
+        } else {
+            System.out.println("No such resume in the storage");
+        }
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
     protected abstract int getIndex(String uuid);
+
+    protected abstract void insertElement(Resume resume, int index);
+
+    protected abstract void fillDeletedElement(int index);
 }
--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -1,7 +1,7 @@
 package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
-import java.util.Arrays;
+
 import static java.util.stream.IntStream.range;
 
 /**
@@ -9,49 +9,14 @@ import static java.util.stream.IntStream.range;
  */
 public class ArrayStorage extends AbstractArrayStorage{
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    public void save(Resume resume) {
-        if (getIndex(resume.getUuid()) >= 0) {
-            System.out.println("The resume is already in the storage");
-            return;
-        }
-        if (size == storage.length) {
-            System.out.println("Storage overflow");
-            return;
-        }
+    @Override
+    protected void insertElement(Resume resume, int index) {
         storage[size] = resume;
-        size++;
     }
 
-    public void update(Resume resume) {
-        int index;
-        if ((index = getIndex(resume.getUuid())) >= 0) {
-            storage[index] = resume;
-        } else {
-            System.out.println("No such resume in the storage");
-        }
-    }
-
-    public void delete(String uuid) {
-        int index;
-        if ((index = getIndex(uuid)) >= 0) {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        } else {
-            System.out.println("No such resume in the storage");
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
+    @Override
+    protected void fillDeletedElement(int index) {
+        storage[index] = storage[size - 1];
     }
 
     protected int getIndex(String uuid) {
