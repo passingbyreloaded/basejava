2d9df615f648d7db3aed77fb42a967e98222ac86
passingbyreloaded <voronayala@mail.ru>
2018-08-09 22:34:59 +0400

    Strategy implementation in serialization refactored

--- test/ru/javawebinar/basejava/storage/ObjectStreamStoragePathTest.java
+++
@@ -1,8 +0,0 @@
-package ru.javawebinar.basejava.storage;
-
-public class ObjectStreamStoragePathTest extends AbstractStorageTest {
-
-    public ObjectStreamStoragePathTest() {
-        super(new PathStorage(STORAGE_DIR.toString(), new ObjectStreamStorage()));
-    }
-}
