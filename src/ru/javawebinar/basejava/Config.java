fbc5f70d9d1e0ec2dfbfdac95ca46b9c229ab6d7
passingbyreloaded <voronayala@mail.ru>
2018-09-04 20:26:00 +0400

    jstl added

--- src/ru/javawebinar/basejava/Config.java
+++ src/ru/javawebinar/basejava/Config.java
@@ -11,7 +11,7 @@ import java.util.Properties;
 
 public class Config {
 
-    private static final File PROPS = new File("C:\\Users\\acidburn\\Desktop\\basejava\\config\\resumes.properties");
+    private static final File PROPS = new File(getHomeDir(), "config\\resumes.properties");
     private static final Config INSTANCE = new Config();
     private final File storageDir;
     private final Storage storage;
@@ -37,5 +37,14 @@ public class Config {
 
     public Storage getStorage() {
         return storage;
+    }
+
+    private static File getHomeDir() {
+        String prop = System.getProperty("homeDir");
+        File homeDir = new File(prop == null ? "." : prop);
+        if (!homeDir.isDirectory()) {
+            throw new IllegalStateException(homeDir + " is not directory");
+        }
+        return homeDir;
     }
 }
---
+++ web/WEB-INF/jsp/fragments/footer.jsp
@@ -0,0 +1,7 @@
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+
+<footer>
+
+    Проект <a href="http://javaops.ru/reg/basejava">Разработка Web приложения База данных резюме</a>
+
+</footer>
